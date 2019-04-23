5bc1927aad1049f82a3a400fdfb22f7f6886d546
Brendan Schiller <schiller.brendan@gmail.com>
2019-04-23 15:48:36 -0600

    mission finished

--- src/components/OurMission/OurMission.js
+++ src/components/OurMission/OurMission.js
@@ -9,19 +9,17 @@ class OurMission extends Component {
         {/* <div className="page-container">
           <h1 className="page-title">Our Mission</h1>
         </div> */}
-        <div className="m-spacing">
-          <div className="m-container">
-            <div className="m-box">
-              <h1>Meet Your New Electric Vehicle</h1>
-              <hr />
-              <p>
-                Boosted is on a mission to make cities feel smaller, campuses
-                more accessible, and commutes more enjoyable. Our boards are
-                designed and built to handle the rigors of daily use while
-                offering an exhilarating ride with unparalleled power and
-                control.{" "}
-              </p>
-            </div>
+
+        <div className="m-container">
+          <div className="m-box">
+            <h1>Meet Your New Electric Vehicle</h1>
+            <hr />
+            <p>
+              Backfire is on a mission to make cities feel smaller, campuses
+              more accessible, and commutes more enjoyable. Our boards are
+              designed and built to handle the rigors of daily use while
+              offering an exhilarating ride with unparalleled power and control.
+            </p>
           </div>
         </div>
       </div>
