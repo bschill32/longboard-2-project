fac0e9d349108751c67aafc902b60a309883d2a3
Brendan Schiller <schiller.brendan@gmail.com>
2019-04-23 14:30:21 -0600

    home page done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,25 @@ import "./Home.css"
 
 class Home extends Component {
   render() {
-    return <div className="home" id="home" />
+    return (
+      <div className="home" id="home">
+        <div>
+          <div className="textBox">
+            <h1>Introducing vehicle-grade electric skateboards</h1>
+            <h2>
+              Rethink how
+              <br />
+              you get there.
+            </h2>
+          </div>
+          <div className="buttonBox">
+            <a href="#boards">
+              <button>Shop Now</button>
+            </a>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
--- src/components/OurMission/OurMission.js
+++ src/components/OurMission/OurMission.js
@@ -5,12 +5,24 @@ import "./OurMission.css"
 class OurMission extends Component {
   render() {
     return (
-      <div id="mission">
-        <div className="page-container">
+      <div className="mission" id="mission">
+        {/* <div className="page-container">
           <h1 className="page-title">Our Mission</h1>
-        </div>
-        <div className="mission-container">
-          <div className="mission-info" />
+        </div> */}
+        <div className="m-spacing">
+          <div className="m-container">
+            <div className="m-box">
+              <h1>Meet Your New Electric Vehicle</h1>
+              <hr />
+              <p>
+                Boosted is on a mission to make cities feel smaller, campuses
+                more accessible, and commutes more enjoyable. Our boards are
+                designed and built to handle the rigors of daily use while
+                offering an exhilarating ride with unparalleled power and
+                control.{" "}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     )
